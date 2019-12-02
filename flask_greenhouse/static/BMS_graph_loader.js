f214aae56a4fc2b57aa92e936d53af1174fe3904
William Kerr <wkerr@ucsc.edu>
2019-12-01 22:23:19 -0800

    Emergency commit

--- flask_greenhouse/static/BMS_graph_loader.js
+++ flask_greenhouse/static/BMS_graph_loader.js
@@ -1,5 +1,86 @@
-async function setup_graph(baseURL) {
+/*In HTML:
+// %3C === <
+// %3E === >
+// Character Translation from Python strings to HTML using the Jinja2 template engine is weird.
+
+*/
+var parameter_placeholder = "%3Cparameter%3E";
+var date_placeholder = "%3Cdate%3E";
+function returnURL(baseURL, date, parameter ) {
+	return baseURL.replace(date_placeholder, date).replace(parameter_placeholder, parameter);
+}
+
+/*
+parseYMDHM: 
+	argument:
+		s: a date string
+	returns:
+		a new JS date object. 
+*/
+function parseYMDHM(s) {
+  var b = s.split(/\D+/);
+  return new Date(b[0], --b[1], b[2], b[3], b[4], b[5]||0, b[6]||0);
+}
+
+/*
+My own date formatter.
+JS does not provide one for you, unlike python. You must write it yourself.
+	Arguments:
+		date: a Date() object from JS.
+	Returns:
+		A string in format: 
+			"%Y-%M-%D %H:%M:%S"
+*/
+function DateFormatter(date) {
+	var day = date.getDate();
+	var month = date.getMonth();
+	var year = date.getFullYear();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var second = date.getSeconds();
+	if(day < 10) day = '0' + day;
+	if(month < 10) month = '0' + month;
+	if(hour < 10) hour = '0' + hour;
+	if(minute < 10) minute = '0' + minute;
+	if(second < 10) second = '0' + second;
+	return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;		
+}
+
+/*
+	Date Comparison Function
+	Arguments:
+		start: Start Date
+		end: End Date
+	Returns: 
+		True if Start Date is Before End Date
+		False if Start Date if After or equal to End Date.
+*/
+
+function dateLessThan(start, end) {
+	console.log("Difference: " + (end - start));
+	if(start.getTime() > end.getTime()) {
+		return false;
+	} else {
+		return true;
+	}
 	
+}	
+
+
+
+
+
+
+
+
+
+/*
+ Main functions
+*/
+
+
+async function setup_graph(baseURL) {
+	$("BMS_form").serializeArray();
 	// retrieve form contents
 	var form = document.getElementById("BMS_form");
 	let start = parseYMDHM(form["startdate"].value);
@@ -71,31 +152,12 @@ async function setup_graph(baseURL) {
 	return false;
 }	
 
-// In HTML:
-// %3C === <
-// %3E === >
-// Character Translation from Python strings to HTML using the Jinja2 template engine is weird.
-var parameter_placeholder = "%3Cparameter%3E";
-var date_placeholder = "%3Cdate%3E";
-function returnURL(baseURL,date,parameter ) {
-	return baseURL.replace(date_placeholder, date).replace(parameter_placeholder, parameter);
-}
 
 
 
 
 
 
-
-function dateLessThan(start, end) {
-	console.log("Difference: " + (end - start));
-	if(start.getTime() > end.getTime()) {
-		return false;
-	} else {
-		return true;
-	}
-	
-}	
 // based on form contents, retrieve data from database from the Base URL provided by the website.
 // RetrieveData() calls the fetch() method. It returns a promise, so you use the await keyword to wait until it is done fetching.
 
@@ -154,10 +216,6 @@ async function RetrieveAllData(start, end, interval, parameter, baseURL) {
 }
 
 
-async function RetrieveData(URL) {
-	const response = await fetch(URL);
-	return response.json();
-}
 
 function ChartIt(data) {
 	var trace = {
@@ -191,36 +249,9 @@ function ChartIt(data) {
 	Plotly.plot('chart', [trace], layout);
 }
 
-function parseYMDHM(s) {
-  var b = s.split(/\D+/);
-  return new Date(b[0], --b[1], b[2], b[3], b[4], b[5]||0, b[6]||0);
-}
 
 
 
-/*
-My own date formatter.
-JS does not provide one for you, unlike python. You must write it yourself.
-	Arguments:
-		date: a Date() object from JS.
-	Returns:
-		A string in format: 
-			"%Y-%M-%D %H:%M:%S"
-*/
-function DateFormatter(date) {
-	var day = date.getDate();
-	var month = date.getMonth();
-	var year = date.getFullYear();
-	var hour = date.getHours();
-	var minute = date.getMinutes();
-	var second = date.getSeconds();
-	if(day < 10) day = '0' + day;
-	if(month < 10) month = '0' + month;
-	if(hour < 10) hour = '0' + hour;
-	if(minute < 10) minute = '0' + minute;
-	if(second < 10) second = '0' + second;
-	return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;		
-}
 
 
 
@@ -280,82 +311,82 @@ function addMinutes(date, minutes) {
 }
 		
 		
-		var str = "{{ url_for('bms.BMS_data_retrieval', date='<date>', parameter='<parameter>') }}"; 
-		// BMS_data_retrieval_URL = BMS_data_retrieval_URL.replace('<date>', '<
-		async function chartIt(BMS_URL) {
-			var BMS_data_retrieval_URL = BMS_URL;
-			var form = document.getElementById("BMS_form");
-			var start = parseYMDHM(form["startdate"].value);
-			var end = parseYMDHM(form["enddate"].value);
-			var interval = form["interval"].value;
-			
-			let start_URL = await BMS_data_retrieval_URL.replace('<date>',DateFormatter(start));
-			console.log('str: ' + str);
-			var x = str.replace('<date>', DateFormatter(start));
-			console.log('x: ' + x);
-			var y = x.replace('<date>',DateFormatter(start));
-			console.log('y: ' + y);
-			// console.log(start_URL);
-			
-			const response = await fetch(start_URL);
-			const myJSON = await response.json();
-			console.log(myJSON);
-			
-			
-			var x_values = [];
-			var y_values = [];
-			
-			// Y value is linear for now.
-			// using the formula y = mx+b 
-			
-			var m = 1;
-			var b = 2;
-			var y = b;
-			
-			while(start < end) {
-				var current_date = DateFormatter(start);
-				console.log(current_date);
-				x_values.push(current_date);
-				y += m;
-				y_values.push(y);
-				
-				start = addMinutes(start, interval);
-			}
-			var trace_name = form["units"].value;
-			var x_title = form["xaxistitle"].value;
-			var y_title = form["yaxistitle"].value;
-			var main_title = form["title"].value;
-			var trace = {
-				x: x_values,
-				y: y_values,
-				name: trace_name,
-				type: 'line'
-			};
-			var layout = {
-				title: {
-					text: main_title
-				},
-				xaxis: {
-					title: {
-						text: x_title
-					},
-					type: 'date' // you should specify that xaxis is a date.
-				},
-				yaxis: {
-					title: {
-						text: y_title
-					},
-					type: 'linear',
-					autorange: true
-				},
-				margin: {
-					t: 0
-				}
-			};
-			// plot the graph. Will be displayed in the 'chart' div.
-			Plotly.plot('chart', [trace], layout);
-			return;
+var str = "{{ url_for('bms.BMS_data_retrieval', date='<date>', parameter='<parameter>') }}"; 
+// BMS_data_retrieval_URL = BMS_data_retrieval_URL.replace('<date>', '<
+async function chartIt(BMS_URL) {
+	var BMS_data_retrieval_URL = BMS_URL;
+	var form = document.getElementById("BMS_form");
+	var start = parseYMDHM(form["startdate"].value);
+	var end = parseYMDHM(form["enddate"].value);
+	var interval = form["interval"].value;
+	
+	let start_URL = await BMS_data_retrieval_URL.replace('<date>',DateFormatter(start));
+	console.log('str: ' + str);
+	var x = str.replace('<date>', DateFormatter(start));
+	console.log('x: ' + x);
+	var y = x.replace('<date>',DateFormatter(start));
+	console.log('y: ' + y);
+	// console.log(start_URL);
+	
+	const response = await fetch(start_URL);
+	const myJSON = await response.json();
+	console.log(myJSON);
+	
+	
+	var x_values = [];
+	var y_values = [];
+	
+	// Y value is linear for now.
+	// using the formula y = mx+b 
+	
+	var m = 1;
+	var b = 2;
+	var y = b;
+	
+	while(start < end) {
+		var current_date = DateFormatter(start);
+		console.log(current_date);
+		x_values.push(current_date);
+		y += m;
+		y_values.push(y);
+		
+		start = addMinutes(start, interval);
+	}
+	var trace_name = form["units"].value;
+	var x_title = form["xaxistitle"].value;
+	var y_title = form["yaxistitle"].value;
+	var main_title = form["title"].value;
+	var trace = {
+		x: x_values,
+		y: y_values,
+		name: trace_name,
+		type: 'line'
+	};
+	var layout = {
+		title: {
+			text: main_title
+		},
+		xaxis: {
+			title: {
+				text: x_title
+			},
+			type: 'date' // you should specify that xaxis is a date.
+		},
+		yaxis: {
+			title: {
+				text: y_title
+			},
+			type: 'linear',
+			autorange: true
+		},
+		margin: {
+			t: 0
 		}
+	};
+	// plot the graph. Will be displayed in the 'chart' div.
+	Plotly.plot('chart', [trace], layout);
+	return;
+}
 		
 		
 		
---
+++ flask_greenhouse/static/old_code.js
@@ -0,0 +1,83 @@
+
+		
+var str = "{{ url_for('bms.BMS_data_retrieval', date='<date>', parameter='<parameter>') }}"; 
+// BMS_data_retrieval_URL = BMS_data_retrieval_URL.replace('<date>', '<
+async function chartIt(BMS_URL) {
+	var BMS_data_retrieval_URL = BMS_URL;
+	var form = document.getElementById("BMS_form");
+	var start = parseYMDHM(form["startdate"].value);
+	var end = parseYMDHM(form["enddate"].value);
+	var interval = form["interval"].value;
+	
+	let start_URL = await BMS_data_retrieval_URL.replace('<date>',DateFormatter(start));
+	console.log('str: ' + str);
+	var x = str.replace('<date>', DateFormatter(start));
+	console.log('x: ' + x);
+	var y = x.replace('<date>',DateFormatter(start));
+	console.log('y: ' + y);
+	// console.log(start_URL);
+	
+	const response = await fetch(start_URL);
+	const myJSON = await response.json();
+	console.log(myJSON);
+	
+	
+	var x_values = [];
+	var y_values = [];
+	
+	// Y value is linear for now.
+	// using the formula y = mx+b 
+	
+	var m = 1;
+	var b = 2;
+	var y = b;
+	
+	while(start < end) {
+		var current_date = DateFormatter(start);
+		console.log(current_date);
+		x_values.push(current_date);
+		y += m;
+		y_values.push(y);
+		
+		start = addMinutes(start, interval);
+	}
+	var trace_name = form["units"].value;
+	var x_title = form["xaxistitle"].value;
+	var y_title = form["yaxistitle"].value;
+	var main_title = form["title"].value;
+	var trace = {
+		x: x_values,
+		y: y_values,
+		name: trace_name,
+		type: 'line'
+	};
+	var layout = {
+		title: {
+			text: main_title
+		},
+		xaxis: {
+			title: {
+				text: x_title
+			},
+			type: 'date' // you should specify that xaxis is a date.
+		},
+		yaxis: {
+			title: {
+				text: y_title
+			},
+			type: 'linear',
+			autorange: true
+		},
+		margin: {
+			t: 0
+		}
+	};
+	// plot the graph. Will be displayed in the 'chart' div.
+	Plotly.plot('chart', [trace], layout);
+	return;
+}
+		
+		
+		
+	
+		
