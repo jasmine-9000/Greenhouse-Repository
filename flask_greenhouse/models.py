c59d475f8d873e51503605b6350b5826f4ffd5f8
William Kerr <wkerr@ucsc.edu>
2019-12-05 13:15:11 -0800

    Fixed comments a little

--- flask_greenhouse/models.py
+++ flask_greenhouse/models.py
@@ -1,6 +1,7 @@
 #########################################################################################################################
 #							IMPORTS																						#
 #########################################################################################################################
+
 from flask_greenhouse import db, admin
 from flask_admin.contrib.sqla import ModelView
 from datetime import datetime
@@ -14,9 +15,9 @@ import json
 # it inherits the TypeDecorator class, so we need to redefine process_bind_param and process_result_value.
 # It needs to save to a SQLlite database as a string. We can pass in a dictionary, and it will convert to string.
 # When we load the value, it will automatically convert it back to dictionary format with json.loads().
+
 class JsonEncodedDict(db.TypeDecorator):
-    """Enables JSON storage by encoding and decoding on the fly.
-	source: https://www.michaelcho.me/article/json-field-type-in-sqlalchemy-flask-python"""
+    """Enables JSON storage by encoding and decoding on the fly."""
     impl = db.Text
 
     def process_bind_param(self, value, dialect):
@@ -31,7 +32,7 @@ class JsonEncodedDict(db.TypeDecorator):
         else:
             return json.loads(value)
 
-#inheritable class for BMS data, Tristar data, Sensor Data, etc.
+# inheritable class for BMS data, Tristar data, Sensor Data, etc.
 class JSONDataEntry(db.Model):
 	id = db.Column(db.Integer, primary_key=True) # a unique ID for our JSON data entry. Every object must have one. It's unique, and it's the primary way the database sorts our JSON data.
 	date_posted = db.Column(db.DateTime(100), nullable=False, default=datetime.utcnow()) # we will keep track of when we received each piece of data. If the method does not have a way of retrieving a date, the default is today.
@@ -42,7 +43,9 @@ class JSONDataEntry(db.Model):
 #########################################################################################################################
 #							ACTUAL ENTRIES																				#
 #########################################################################################################################
+
 # same as JSONDataEntry, but tablename is different for every one.
+
 class BMSDataentry(JSONDataEntry):
 	__tablename__ = 'BMS Data'
 	
@@ -51,7 +54,17 @@ class TristarDataEntry(JSONDataEntry):
 
 class TristarDailyDataEntry(JSONDataEntry):
 	__tablename__ = 'Tristar Daily Data'
-
+	
+	
+#############################################################################################################################
+#									ADMIN MODEL VIEWS																		#
+#############################################################################################################################
 admin.add_view(ModelView(BMSDataentry, db.session))
 admin.add_view(ModelView(TristarDataEntry, db.session))
 admin.add_view(ModelView(TristarDailyDataEntry, db.session))
+
+#############################################################################################################################
+#									SOURCES																					#
+#############################################################################################################################
+
+# source: https://www.michaelcho.me/article/json-field-type-in-sqlalchemy-flask-python
